1a99da80837c3ba3d88d266c6f750a37996e1c08
dmmaia <david_mmaia@hotmail.com>
2026-06-01 23:56:58 -0300

    fear: basic texture optimization added

--- src/services/gltf-optimizer.service.ts
+++ src/services/gltf-optimizer.service.ts
@@ -2,6 +2,7 @@ import { NodeIO } from '@gltf-transform/core';
 import { KHRDracoMeshCompression } from '@gltf-transform/extensions';
 import { draco } from '@gltf-transform/functions';
 import draco3d from 'draco3dgltf';
+import sharp from 'sharp'
 
 const encoderModule = await draco3d.createEncoderModule();
 const decoderModule = await draco3d.createDecoderModule();
@@ -16,6 +17,26 @@ export default {
     });
 
     const document = await io.readBinary(new Uint8Array(glbBuffer));
+
+    const textures = document.getRoot().listTextures()
+    
+    for(const texture of textures){
+      const originalBuffer = texture.getImage()
+      if(!originalBuffer) continue
+
+      const optimizedBuffer = await sharp(originalBuffer)
+        .resize({
+          width: 1024,
+          height: 1024,
+          fit: "inside",
+          withoutEnlargement: true,
+        })
+        .webp({quality: 75})
+        .toBuffer();
+
+      texture.setImage(optimizedBuffer)
+      texture.setMimeType('image/webp')
+    }
 
     await document.transform(
       draco({
